cd7a10caab14699e202f07226b25e566451dcbeb
Emilie Maniglier <emilie.maniglier@kinoba.fr>
2024-06-13 09:34:37 +0200

    feat(router): add private route

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import { Router, Route } from 'svelte-navigator';
 
-  import { routes } from '@/router';
   import { DesignSystemPage, HomePage, LoginPage } from '@/components';
+  import { routes, PrivateRoute } from '@/router';
 </script>
 
 <main>
@@ -10,7 +10,10 @@
     <!-- TODO : Show DesignSystem Route only in dev mode -->
     <Route path={routes.designSystem.path} component={DesignSystemPage} />
 
-    <Route path={routes.home.path} component={HomePage} />
     <Route path={routes.login.path} component={LoginPage} />
+
+    <PrivateRoute path={routes.home.path} let:location>
+      <HomePage />
+    </PrivateRoute>
   </Router>
 </main>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,1 +1,3 @@
 export { routes } from '@/router/routes';
+export { default as PrivateRoute } from '@/router/PrivateRoute.svelte';
+export { default as PrivateRouteGuard } from '@/router/PrivateRouteGuard.svelte';
